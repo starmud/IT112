2e4194473b23c028ad621b56b5db3bc9e93905d9
David <nowolf@gmail.com>
2025-05-26 17:26:17 -0700

    classwork

---
+++ lib.py
@@ -0,0 +1,19 @@
+# python file following class example
+import json
+
+data1 = {
+
+    "name": "John Appleseed", #string value
+    "age": 30, #integer value
+    "city": "New York", #string value
+    "interests":["reading", "traveling", "swimming"], #list of strings
+    "is_student": False #boolean value
+
+}
+
+with open('data.json', 'w') as json_file:
+
+    json.dump(data1, json_file, indent=4)
+# This code creates a JSON file named 'data.json' with the specified data structure.
+
+print("JSON file created successfully")
